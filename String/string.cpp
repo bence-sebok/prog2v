c9f1bec53c71ffe2486b0e3632ce3f7d0605db1b
Bence Sebők <bence.sebok.personal@gmail.com>
2019-03-24 19:51:31 +0100

    Add homemade String class (.cpp, .h)

---
+++ String/string.cpp
@@ -0,0 +1,143 @@
+﻿#include "string.h"
+#include <cstring>
+
+namespace HomeMadeString {
+
+	String String::concatenate(const String& string1, const String& string2)
+	{
+		String s;
+		s.elementNum = string1.elementNum + string2.elementNum;
+		if (s.elementNum == 0)
+		{
+			s.pData = nullptr;
+			return s;
+		}
+
+		s.pData = new char[s.elementNum + 1];
+
+		for (unsigned i = 0; i<string1.elementNum; i++)
+		{
+			s.pData[i] = string1.pData[i];
+		}
+	
+		for (unsigned j = string1.elementNum; j<=s.elementNum; j++)
+		{
+			s.pData[j] = string2.pData[j - string1.elementNum];
+		}
+
+		return s;
+	}
+
+	bool String::compare(const String& string1,const String& string2)
+	{
+		if (string1.elementNum != string2.elementNum)
+		{
+			return false;
+		}
+
+		for (unsigned i = 0; i < string1.elementNum; i++)
+		{
+			if (string1.pData[i] != string2.pData[i])
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	void String::copy(String& destination, const String& source)
+	{
+		delete[] destination.pData;
+
+		destination.elementNum = source.elementNum;
+
+		if (destination.elementNum == 0)
+		{
+			destination.pData = nullptr;
+			return;
+		}
+
+		destination.pData = new char[destination.elementNum + 1];
+		for (unsigned i = 0; i <= destination.elementNum; i++)
+		{
+			destination.pData[i] = source.pData[i];
+		}
+	}
+
+
+	String::String(): elementNum(0), pData(NULL) {}
+
+	String::String(const String& other): pData(NULL)
+	{
+		copy(*this, other);
+	}
+
+	String::String(const char* str)
+	{
+		elementNum = strlen(str);
+		
+		// 0 elemű sztring kezelese
+		if(elementNum == 0)
+		{
+			pData = nullptr;
+			return;
+		}
+
+		pData = new char[elementNum + 1];
+		strcpy(pData, str);
+	}
+
+	String::String(char c, unsigned times): elementNum(times)
+	{
+		elementNum = times;
+
+		if(elementNum == 0)
+		{
+			pData = nullptr;
+			return;
+		}
+
+		pData = new char[elementNum+1];
+		for (unsigned i = 0; i < times; i++)
+			pData[i] = c;
+		pData[elementNum] = '\0';
+	}
+
+	String::~String()
+	{
+		delete[] pData;
+	}
+
+	const char* String::getCString() const
+	{
+		return pData;
+	}
+
+	int String::getLength() const
+	{
+		return elementNum;
+	}
+
+	void String::print(std::ostream& os) const
+	{
+		if (pData == nullptr) 
+		{
+			return;
+		}
+		os << pData;
+	}
+
+	char String::at(unsigned pos) const
+	{
+		if(pos >= 0 && pos < elementNum)
+		{
+			return pData[pos];
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+}
---
+++ String/string.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+#include <iostream>
+
+namespace HomeMadeString
+{
+	class String
+	{
+		// A karakterek aktuális száma. A NULL terminátor nem számít bele, de ugyanúgy tároljuk.
+		unsigned elementNum; // unsigned === unsigned int
+
+		// A karaktereket tartalmazó memóriaterületre mutató pointer
+		char* pData;
+	public:
+		// Összefűz két stringet, és visszatér vele
+		static String concatenate(const String&, const String&);
+
+		// Összehasonlít két stringet
+		static bool compare(const String&, const String&);
+
+		// A második stringet az első stringbe másolja
+		static void copy(String&, const String&);
+
+		// Argumentum nélküli konstruktor:
+		String();
+
+		// Másoló konstruktor
+		String(const String&);
+
+		// Egy NULL végű stringet váró konstruktor
+		String(const char*);
+
+		// Egy karaktert és egy előjel nélküli egészet (times) váró konstruktor,
+		// amely times darab c karakterrel inicializálja a stringet
+		String(char, unsigned);
+
+		// A destruktor
+		~String();
+
+		// Visszaadja a tárolt C stringet
+		const char* getCString() const;
+
+		// Visszatér a string hosszával
+		int getLength() const;
+
+		// Kiírja a stringet a megadott kimeneti adatfolyamba (A 'cout' ostream típusú.
+		// A .h állományban nem használunk using namespace-t, mert nem tudjuk, hova lesz
+		// beépítve, és ott milyen hatása lesz. Ezért kiíjuk az std::-t. Ez a .cpp állományban
+		// már nem kell, ott használhatjuk a using namespace std utasítást):
+		void print(std::ostream&) const;
+
+		// Visszaadja a megadott pozícióban lévo karaktert, egyébként nullát:
+		char at(unsigned) const;
+	};
+}
